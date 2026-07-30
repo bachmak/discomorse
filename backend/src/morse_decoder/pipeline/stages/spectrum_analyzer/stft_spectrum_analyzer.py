Fix normalization

backend/src/morse_decoder/pipeline/stages/spectrum_analyzer/stft_spectrum_analyzer.py
@@ -58,7 +58,7 @@ class STFTSpectrumAnalyzer(SpectrumAnalyzer):
             window=self._window,
         )
         raw_magnitudes = np.abs(matrix)
-        normalized_magnitudes = raw_magnitudes / self._amplitude_scale
+        normalized_magnitudes = raw_magnitudes * self._amplitude_scale
         return normalized_magnitudes
 
     def _single_step_dt(self) -> datetime.timedelta: